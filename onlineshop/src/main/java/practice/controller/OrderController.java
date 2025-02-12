b3e125291eec0e20ddb9e9913ca84022ac663efa
Oleg <oleg211190gm@yandex.ru>
2025-02-12 10:20:13 +0300

    changes

--- onlineshop/src/main/java/practice/controller/OrderController.java
+++ onlineshop/src/main/java/practice/controller/OrderController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import practice.model.order.OrderRequest;
 import practice.model.order.OrderResponse;
 import practice.service.OrderService;
@@ -33,6 +32,6 @@ public class OrderController {
     @PostMapping
     public ResponseEntity<Void> createOrder(@RequestBody String orderJson) throws JsonProcessingException {
         orderService.createOrder(objectMapper.readValue(orderJson, OrderRequest.class));
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }
